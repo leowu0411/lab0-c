b9163da422cbeb220b94ae9e9f3c70b1bf4d9c0b
Po-Hao Wu <leowu0411@gmail.com>
2025-03-07 19:47:47 +0800

    Add feature to remove outliers in measurements
    Modify measurement process to discard the top 5% of outliers
    by sorting and cropping long execution times. Ensure more
    reliable t-test results. This is a naive implementation
    to verify the effectiveness of the modification.
    
    Change-Id: I112a6616f920b5d26aaf3774a504b68ccb4dcb8a

--- dudect/fixture.c
+++ dudect/fixture.c
@@ -26,233 +26,167 @@
  *    variable time.
  */
 
-#include <assert.h>
-#include <math.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-#include "../console.h"
-#include "../random.h"
-
-#include "constant.h"
-#include "fixture.h"
-#include "ttest.h"
-
-#define ENOUGH_MEASURE 10000
-#define TEST_TRIES 10
-
-/* Number of percentiles to calculate */
-#define NUM_PERCENTILES (100)
-#define DUDECT_TESTS (NUM_PERCENTILES + 1)
-
-static t_context_t *ctxs[DUDECT_TESTS];
-
-/* threshold values for Welch's t-test */
-enum {
-    t_threshold_bananas = 500, /* Test failed with overwhelming probability */
-    t_threshold_moderate = 10, /* Test failed */
-};
-
-static void __attribute__((noreturn)) die(void)
-{
-    exit(111);
-}
-
-static int64_t percentile(const int64_t *a_sorted, double which, size_t size)
-{
-    assert(which >= 0 && which <= 1.0);
-    size_t pos = (size_t) (which * size);
-    return a_sorted[pos];
-}
-
-/* leverages the fact that comparison expressions return 1 or 0. */
-static int cmp(const void *aa, const void *bb)
-{
-    int64_t a = *(const int64_t *) aa, b = *(const int64_t *) bb;
-    return (a > b) - (a < b);
-}
-
-/* This function is used to set different thresholds for cropping measurements.
- * To filter out slow measurements, we keep only the fastest ones by a
- * complementary exponential decay scale as thresholds for cropping
- * measurements: threshold(x) = 1 - 0.5^(10 * x / N_MEASURES), where x is the
- * counter of the measurement.
- */
-static void prepare_percentiles(int64_t *exec_times, int64_t *percentiles)
-{
-    qsort(exec_times, N_MEASURES, sizeof(int64_t), cmp);
-
-    for (size_t i = 0; i < NUM_PERCENTILES; i++) {
-        percentiles[i] = percentile(
-            exec_times, 1 - (pow(0.5, 10 * (double) (i + 1) / NUM_PERCENTILES)),
-            N_MEASURES);
-    }
-}
-
-static void differentiate(int64_t *exec_times,
-                          const int64_t *before_ticks,
-                          const int64_t *after_ticks)
-{
-    for (size_t i = 0; i < N_MEASURES; i++)
-        exec_times[i] = after_ticks[i] - before_ticks[i];
-}
-
-static void update_statistics(const int64_t *exec_times,
-                              uint8_t *classes,
-                              int64_t *percentiles)
-{
-    for (size_t i = 0; i < N_MEASURES; i++) {
-        int64_t difference = exec_times[i];
-        /* CPU cycle counter overflowed or dropped measurement */
-        if (difference <= 0)
-            continue;
-
-        /* do a t-test on the execution time */
-        t_push(ctxs[0], difference, classes[i]);
-
-        /* t-test on cropped execution times, for several cropping thresholds.
-         */
-        for (size_t j = 0; j < NUM_PERCENTILES; j++) {
-            if (difference < percentiles[j]) {
-                t_push(ctxs[j + 1], difference, classes[i]);
-            }
-        }
-    }
-}
-
-static t_context_t *max_test()
-{
-    size_t max_idx = 0;
-    double max_t = 0.0f;
-    for (size_t i = 0; i < NUM_PERCENTILES + 1; i++) {
-        double t = fabs(t_compute(ctxs[i]));
-        if (t > max_t) {
-            max_t = t;
-            max_idx = i;
-        }
-    }
-    return ctxs[max_idx];
-}
-
-static bool report(void)
-{
-    t_context_t *t = max_test();
-    double number_traces_max_t = t->n[0] + t->n[1];
-
-    printf("\033[A\033[2K");
-    printf("measure: %7.2lf M, ", (number_traces_max_t / 1e6));
-    if (number_traces_max_t < ENOUGH_MEASURE) {
-        printf("not enough measurements (%.0f still to go).\n",
-               ENOUGH_MEASURE - number_traces_max_t);
-        return false;
-    }
-
-    double max_t = fabs(t_compute(t));
-    double max_tau = max_t / sqrt(number_traces_max_t);
-
-    /* max_t: the t statistic value
-     * max_tau: a t value normalized by sqrt(number of measurements).
-     *          this way we can compare max_tau taken with different
-     *          number of measurements. This is sort of "distance
-     *          between distributions", independent of number of
-     *          measurements.
-     * (5/tau)^2: how many measurements we would need to barely
-     *            detect the leak, if present. "barely detect the
-     *            leak" = have a t value greater than 5.
-     */
-    printf("max t: %+7.2f, max tau: %.2e, (5/tau)^2: %.2e.\n", max_t, max_tau,
-           (double) (5 * 5) / (double) (max_tau * max_tau));
-
-    /* Definitely not constant time */
-    if (max_t > t_threshold_bananas)
-        return false;
-
-    /* Probably not constant time. */
-    if (max_t > t_threshold_moderate)
-        return false;
-
-    /* For the moment, maybe constant time. */
-    return true;
-}
-
-static bool doit(int mode)
-{
-    int64_t *before_ticks = calloc(N_MEASURES + 1, sizeof(int64_t));
-    int64_t *after_ticks = calloc(N_MEASURES + 1, sizeof(int64_t));
-    int64_t *exec_times = calloc(N_MEASURES, sizeof(int64_t));
-    uint8_t *classes = calloc(N_MEASURES, sizeof(uint8_t));
-    uint8_t *input_data = calloc(N_MEASURES * CHUNK_SIZE, sizeof(uint8_t));
-    int64_t *percentiles = calloc(NUM_PERCENTILES, sizeof(int64_t));
-
-    if (!before_ticks || !after_ticks || !exec_times || !classes ||
-        !input_data || !percentiles) {
-        die();
-    }
-
-    prepare_inputs(input_data, classes);
-
-    bool ret = measure(before_ticks, after_ticks, input_data, mode);
-    differentiate(exec_times, before_ticks, after_ticks);
-    prepare_percentiles(exec_times, percentiles);
-    update_statistics(exec_times, classes, percentiles);
-    ret &= report();
-
-    free(before_ticks);
-    free(after_ticks);
-    free(exec_times);
-    free(classes);
-    free(input_data);
-    free(percentiles);
-
-    return ret;
-}
-
-static void init_once(void)
-{
-    init_dut();
-    for (size_t i = 0; i < DUDECT_TESTS; i++) {
-        /* Check if ctxs[i] is unallocated to prevent repeated memory
-         * allocations.
-         */
-        if (!ctxs[i]) {
-            ctxs[i] = malloc(sizeof(t_context_t));
-            t_init(ctxs[i]);
-        }
-    }
-}
-
-static bool test_const(char *text, int mode)
-{
-    bool result = false;
-
-    init_once();
-
-    for (int cnt = 0; cnt < TEST_TRIES; ++cnt) {
-        printf("Testing %s...(%d/%d)\n\n", text, cnt, TEST_TRIES);
-        for (int i = 0; i < ENOUGH_MEASURE / (N_MEASURES - DROP_SIZE * 2) + 1;
-             ++i)
-            result = doit(mode);
-        printf("\033[A\033[2K\033[A\033[2K");
-        if (result)
-            break;
-    }
-
-    for (size_t i = 0; i < DUDECT_TESTS; i++) {
-        free(ctxs[i]);
-        ctxs[i] = NULL;
-    }
-
-    return result;
-}
-
-#define DUT_FUNC_IMPL(op)                \
-    bool is_##op##_const(void)           \
-    {                                    \
-        return test_const(#op, DUT(op)); \
-    }
-
-#define _(x) DUT_FUNC_IMPL(x)
-DUT_FUNCS
-#undef _
+ #include <assert.h>
+ #include <math.h>
+ #include <stdint.h>
+ #include <stdio.h>
+ #include <stdlib.h>
+ #include <string.h>
+ 
+ #include "../console.h"
+ #include "../random.h"
+ 
+ #include "constant.h"
+ #include "fixture.h"
+ #include "ttest.h"
+ 
+ #define ENOUGH_MEASURE 10000
+ #define TEST_TRIES 10
+ #define FINAL_SIZE (int) ((N_MEASURES - DROP_SIZE * 2) * 0.95)
+ 
+ static t_context_t *t;
+ 
+ /* threshold values for Welch's t-test */
+ enum {
+     t_threshold_bananas = 500, /* Test failed with overwhelming probability */
+     t_threshold_moderate = 10, /* Test failed */
+ };
+ 
+ static void __attribute__((noreturn)) die(void)
+ {
+     exit(111);
+ }
+ 
+ static void differentiate(int64_t *exec_times,
+                           const int64_t *before_ticks,
+                           const int64_t *after_ticks)
+ {
+     for (size_t i = 0; i < N_MEASURES; i++)
+         exec_times[i] = after_ticks[i] - before_ticks[i];
+ }
+ 
+ static void update_statistics(const int64_t *exec_times, uint8_t *classes)
+ {
+     for (size_t i = (2 * DROP_SIZE); i < FINAL_SIZE + (2 * DROP_SIZE); i++) {
+         int64_t difference = exec_times[i];
+         /* CPU cycle counter overflowed or dropped measurement */
+         if (difference <= 0)
+             continue;
+ 
+         /* do a t-test on the execution time */
+         t_push(t, difference, classes[i]);
+     }
+ }
+ 
+ static bool report(void)
+ {
+     double max_t = fabs(t_compute(t));
+     double number_traces_max_t = t->n[0] + t->n[1];
+     double max_tau = max_t / sqrt(number_traces_max_t);
+ 
+     printf("\033[A\033[2K");
+     printf("meas: %7.2lf M, ", (number_traces_max_t / 1e6));
+     if (number_traces_max_t < ENOUGH_MEASURE) {
+         printf("not enough measurements (%.0f still to go).\n",
+                ENOUGH_MEASURE - number_traces_max_t);
+         return false;
+     }
+ 
+     /* max_t: the t statistic value
+      * max_tau: a t value normalized by sqrt(number of measurements).
+      *          this way we can compare max_tau taken with different
+      *          number of measurements. This is sort of "distance
+      *          between distributions", independent of number of
+      *          measurements.
+      * (5/tau)^2: how many measurements we would need to barely
+      *            detect the leak, if present. "barely detect the
+      *            leak" = have a t value greater than 5.
+      */
+     printf("max t: %+7.2f, max tau: %.2e, (5/tau)^2: %.2e.\n", max_t, max_tau,
+            (double) (5 * 5) / (double) (max_tau * max_tau));
+ 
+     /* Definitely not constant time */
+     if (max_t > t_threshold_bananas)
+         return false;
+ 
+     /* Probably not constant time. */
+     if (max_t > t_threshold_moderate)
+         return false;
+ 
+     /* For the moment, maybe constant time. */
+     return true;
+ }
+ 
+ int compare(const void *a, const void *b)
+ {
+     return (*(int64_t *) a - *(int64_t *) b);
+ }
+ 
+ static void crop_outliner(int64_t *exec_times)
+ {
+     qsort(exec_times, N_MEASURES, sizeof(int64_t), compare);
+ }
+ 
+ static bool doit(int mode)
+ {
+     int64_t *before_ticks = calloc(N_MEASURES + 1, sizeof(int64_t));
+     int64_t *after_ticks = calloc(N_MEASURES + 1, sizeof(int64_t));
+     int64_t *exec_times = calloc(N_MEASURES, sizeof(int64_t));
+     uint8_t *classes = calloc(N_MEASURES, sizeof(uint8_t));
+     uint8_t *input_data = calloc(N_MEASURES * CHUNK_SIZE, sizeof(uint8_t));
+ 
+     if (!before_ticks || !after_ticks || !exec_times || !classes ||
+         !input_data) {
+         die();
+     }
+ 
+     prepare_inputs(input_data, classes);
+ 
+     bool ret = measure(before_ticks, after_ticks, input_data, mode);
+     differentiate(exec_times, before_ticks, after_ticks);
+     crop_outliner(exec_times);
+     update_statistics(exec_times, classes);
+     ret &= report();
+ 
+     free(before_ticks);
+     free(after_ticks);
+     free(exec_times);
+     free(classes);
+     free(input_data);
+ 
+     return ret;
+ }
+ 
+ static void init_once(void)
+ {
+     init_dut();
+     t_init(t);
+ }
+ 
+ static bool test_const(char *text, int mode)
+ {
+     bool result = false;
+     t = malloc(sizeof(t_context_t));
+ 
+     for (int cnt = 0; cnt < TEST_TRIES; ++cnt) {
+         printf("Testing %s...(%d/%d)\n\n", text, cnt, TEST_TRIES);
+         init_once();
+         for (int i = 0; i < ENOUGH_MEASURE / FINAL_SIZE + 1; ++i)
+             result = doit(mode);
+         printf("\033[A\033[2K\033[A\033[2K");
+         if (result)
+             break;
+     }
+     free(t);
+     return result;
+ }
+ 
+ #define DUT_FUNC_IMPL(op)                \
+     bool is_##op##_const(void)           \
+     {                                    \
+         return test_const(#op, DUT(op)); \
+     }
+ 
+ #define _(x) DUT_FUNC_IMPL(x)
+ DUT_FUNCS
+ #undef _
